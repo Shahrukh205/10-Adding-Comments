b53c3af1351d0a8fe49f19ed6118b27b9ca9f420
Shahrukh Ali <160590321+Shahrukh205@users.noreply.github.com>
2024-04-21 11:34:46 +0500

    Add files via upload

---
+++ Program_2.js
@@ -0,0 +1,5 @@
+"use strict";
+// Program Written by: Shahrukh
+// Current Date: 21-4-2024
+// This program will add two digits.
+console.log(2 + 2);
---
+++ Program_2.ts
@@ -0,0 +1,10 @@
+
+
+
+
+// Program Written by: Shahrukh
+// Current Date: 21-4-2024
+
+// This program will add two digits.
+
+console.log(2+2);
